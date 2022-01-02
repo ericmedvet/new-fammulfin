d1d12ae34d6ce0913ca751473b97d4602e28bf61
Eric Medvet <eric.medvet@gmail.com>
2022-01-02 21:33:16 +0100

    Moved to Spring.

---
+++ src/test/java/it/fammulfin/newfammulfin/NewfammulfinApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fammulfin.newfammulfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfammulfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
